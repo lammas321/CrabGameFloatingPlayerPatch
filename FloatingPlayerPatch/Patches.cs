398913646f1fe8853cfadc4ac727ff3318fc1198
lammas123 <48428621+lammas321@users.noreply.github.com>
2024-08-16 08:19:55 -0500

    Update v1.1.0

--- FloatingPlayerPatch/Patches.cs
+++ FloatingPlayerPatch/Patches.cs
@@ -1,18 +1,25 @@
 ﻿using HarmonyLib;
+using SteamworksNative;
 using System;
+using TMPro;
+using UnityEngine;
 
 namespace FloatingPlayerPatch
 {
     internal static class Patches
     {
-        // Anti Bepinex detection (thanks o7Moon https://github.com/o7Moon/CrabGame.AntiAntiBepinex)
+        //   Anti Bepinex detection (Thanks o7Moon: https://github.com/o7Moon/CrabGame.AntiAntiBepinex)
         [HarmonyPatch(typeof(EffectManager), nameof(EffectManager.Method_Private_Void_GameObject_Boolean_Vector3_Quaternion_0))] // Ensures effectSeed is never set to 4200069 (if it is, modding has been detected)
         [HarmonyPatch(typeof(LobbyManager), nameof(LobbyManager.Method_Private_Void_0))] // Ensures connectedToSteam stays false (true means modding has been detected)
-        //[HarmonyPatch(typeof(SnowSpeedModdingDetector), nameof(SnowSpeedModdingDetector.Method_Private_Void_0))] // Would ensure snowSpeed is never set to Vector3.zero, but it is immediately set back to Vector3.one due to an accident on Dani's part lol
+        //[HarmonyPatch(typeof(SnowSpeedModdingDetector), nameof(SnowSpeedModdingDetector.Method_Private_Void_0))] // Would ensure snowSpeed is never set to Vector3.zero (though it is immediately set back to Vector3.one due to an accident on Dani's part lol)
         [HarmonyPrefix]
-        public static bool PreBepinexDetection() => false;
+        public static bool PreBepinexDetection()
+            => false;
 
-        // Floating player patch
+
+        //   Server Side Floating Player Patch
+        // Broadcasts all received player positions to all other players, fixing the issue
+        // where some players will appear to float for others, but never you as the host.
         [HarmonyPatch(typeof(ServerHandle), nameof(ServerHandle.PlayerPosition))]
         [HarmonyPostfix]
         internal static void PostServerHandlePlayerPosition(ulong param_0, Packet param_1)
@@ -26,6 +33,27 @@ namespace FloatingPlayerPatch
         {
             if (SteamManager.Instance.IsLobbyOwner())
                 ServerSend.PlayerRotation(param_0, BitConverter.ToSingle(param_1.field_Private_ArrayOf_Byte_0, 8), BitConverter.ToSingle(param_1.field_Private_ArrayOf_Byte_0, 12));
+        }
+
+
+        //   Client Side Laggy Movement Patch
+        // This will make you treat the host as if they're always nearby, meaning that you'll
+        // send them the same frequency of position and rotation updates, regardless of how far
+        // away you are. This way, you won't appear as laggy to nearby players if the host is
+        // far away, but others without the mod that are nearby will still appear laggy to you.
+        [HarmonyPatch(typeof(PlayerCommunication), nameof(PlayerCommunication.Method_Private_Void_Vector3_EnumNPublicSealedvaClMeFaAn5vUnique_0))]
+        [HarmonyPrefix]
+        internal static void PrePlayerCommunicationUpdatePosition(PlayerCommunication __instance)
+        {
+            if (!SteamManager.Instance.IsLobbyOwner())
+                __instance.idToDistance[SteamManager.Instance.field_Private_CSteamID_1.m_SteamID] = PlayerCommunicationDistance.Close;
+        }
+        [HarmonyPatch(typeof(PlayerCommunication), nameof(PlayerCommunication.Method_Private_Void_Single_Single_EnumNPublicSealedvaClMeFaAn5vUnique_0))]
+        [HarmonyPrefix]
+        internal static void PrePlayerCommunicationUpdateRotation(PlayerCommunication __instance)
+        {
+            if (!SteamManager.Instance.IsLobbyOwner())
+                __instance.idToDistance[SteamManager.Instance.field_Private_CSteamID_1.m_SteamID] = PlayerCommunicationDistance.Close;
         }
     }
 }
--- FloatingPlayerPatch/Plugin.cs
+++ FloatingPlayerPatch/Plugin.cs
@@ -1,14 +1,19 @@
 using BepInEx;
 using BepInEx.IL2CPP;
 using HarmonyLib;
+using ModList;
 
 namespace FloatingPlayerPatch
 {
-    [BepInPlugin("lammas123.FloatingPlayerPatch", "FloatingPlayerPatch", MyPluginInfo.PLUGIN_VERSION)]
+    [BepInPlugin($"lammas123.{MyPluginInfo.PLUGIN_NAME}", MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+    [BepInDependency("lammas123.ModList", BepInDependency.DependencyFlags.SoftDependency)]
     public class FloatingPlayerPatch : BasePlugin
     {
         public override void Load()
         {
+            if (IL2CPPChainloader.Instance.Plugins.ContainsKey("lammas123.ModList"))
+                ModList.ModList.AddMod($"lammas123.{MyPluginInfo.PLUGIN_NAME}");
+            
             Harmony.CreateAndPatchAll(typeof(Patches));
             Log.LogInfo($"Loaded [{MyPluginInfo.PLUGIN_NAME} {MyPluginInfo.PLUGIN_VERSION}]");
         }
